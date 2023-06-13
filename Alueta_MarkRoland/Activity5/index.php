6b71b5110485b06b672ce2f55944c345b8e078bf
Mark Roland Alueta <markintoshroland@gmail.com>
2023-06-13 08:53:54 +0800

    Fix: Removed commits unrelated to Activity 5, Removed new lines before the </pre> tags

--- Alueta_MarkRoland/Activity5/index.php
+++ Alueta_MarkRoland/Activity5/index.php
@@ -15,7 +15,7 @@ $item_3 = 300; // salt
 
 function buyItem ($item, $quantity) {
     global $item_1, $item_2, $item_3;
-    
+
     if($quantity <= 0){
         return "Please enter a valid quantity <br>";
     }
@@ -23,15 +23,15 @@ function buyItem ($item, $quantity) {
     switch($item){
         case "bread": return 
             $item_1 > 0
-                ? ($quantity <= $item_1 ? "<pre> Item 1: Bread \t ($quantity) qty \t | Remaining Stocks = " . ($item_1 -= $quantity) . "\n </pre>" : "Not enough stocks for Bread! <br>")
+                ? ($quantity <= $item_1 ? "<pre> Item 1: Bread \t ($quantity) qty \t | Remaining Stocks = " . ($item_1 -= $quantity) . "</pre>" : "Not enough stocks for Bread! <br>")
                 : "This item (Bread) is sold out! <br>";
         case "eggs": return
             $item_2 > 0
-                ? ($quantity <= $item_2 ? "<pre> Item 2: Eggs \t ($quantity) qty \t | Remaining Stocks = " . ($item_2 -= $quantity) . "\n </pre>" : "Not enough stocks for Eggs! <br>")
+                ? ($quantity <= $item_2 ? "<pre> Item 2: Eggs \t ($quantity) qty \t | Remaining Stocks = " . ($item_2 -= $quantity) . "</pre>" : "Not enough stocks for Eggs! <br>")
                 : "This item (Eggs) is sold out! <br>";
         case "salt": return
             $item_3 > 0
-                ? ($quantity <= $item_3 ? "<pre> Item 3: Salt \t ($quantity) qty \t | Remaining Stocks = " . ($item_3 -= $quantity) . "\n </pre>" : "Not enough stocks for Salt! <br>")
+                ? ($quantity <= $item_3 ? "<pre> Item 3: Salt \t ($quantity) qty \t | Remaining Stocks = " . ($item_3 -= $quantity) . "</pre>" : "Not enough stocks for Salt! <br>")
                 : "This item (Salt) is sold out! <br>";
         default: return "Item is not on the Inventory! <br>";
     }
